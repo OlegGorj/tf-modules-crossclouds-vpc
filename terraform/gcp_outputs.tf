65b47d51b3aa7877f7d96b4774599ede317d2360
Oleg Gorodnitchi <oleg.gorodnitchi@sk.ibm.com>
2018-07-04 11:59:33 -0400

    initial commits and fixes

---
+++ terraform/gcp_outputs.tf
@@ -0,0 +1,12 @@
+/*
+ * Terraform output variables for GCP.
+ */
+
+output "gcp_instance_external_ip" {
+  value =
+"${google_compute_instance.gcp-vm.network_interface.0.access_config.0.assigned_nat_ip}"
+}
+
+output "gcp_instance_internal_ip" {
+  value = "${google_compute_instance.gcp-vm.network_interface.0.address}"
+}
