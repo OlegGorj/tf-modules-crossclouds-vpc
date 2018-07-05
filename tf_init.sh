7ac5cf1754d2b5e382b39030b370f2566bcc2bb0
Oleg Gorodnitchi <oleg.gorodnitchi@sk.ibm.com>
2018-07-05 15:51:53 -0400

    init script - aws part

--- tf_init.sh
+++ tf_init.sh
@@ -119,21 +119,8 @@ then
   addTFVar "${TFVARS_FILE_PATH}" "aws_credentials_file_path" "${CREDS_FILE}"
 fi
 addTFVar "${TFVARS_FILE_PATH}" "environment" "${ENV}"
-#addTFVar "${TFVARS_FILE_PATH}" "tf_ssh_key" "${TF_VAR_ssh_key}"
-#addTFVar "${TFVARS_FILE_PATH}" "tf_ssh_private_key_file" "${TF_VAR_ssh_private_key}"
 addTFVar "${TFVARS_FILE_PATH}" "source_ranges_ips" "${ALLOWED_IP_RANGE}"
 
-#cat << EOF > $TF_ENV.tfvars
-#env = "$TF_ENV"
-#region = "$GOOGLE_REGION"
-#billing_account = "$TF_VAR_billing_account"
-#org_id = "$TF_VAR_org_id"
-#domain = "$GOOGLE_ADMIN_DOMAIN"
-#admin_account="$GOOGLE_ADMIN_ACCOUNT"
-#g_folder = "$FULL_FOLDER_ID"
-#g_folder_id = "$FOLDER_ID"
-#admin_project = "$TF_PROJECT_ID"
-#source_ranges_ips = "$ALLOWED_IP_RANGE/32"
-#tf_ssh_key = "$TF_VAR_ssh_key"
-#tf_ssh_private_key_file = "$TF_VAR_ssh_private_key"
-#EOF
+# TODO: add keys
+#addTFVar "${TFVARS_FILE_PATH}" "tf_ssh_key" "${TF_VAR_ssh_key}"
+#addTFVar "${TFVARS_FILE_PATH}" "tf_ssh_private_key_file" "${TF_VAR_ssh_private_key}"
